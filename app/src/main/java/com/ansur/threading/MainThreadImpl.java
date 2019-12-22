c18b4d456e134d787a41016f8d86de7c2ad4c3b3
simonrichards <simonrich@gmail.com>
2019-12-22 18:35:32 +0100

    Update recyclerview dependency

--- app/src/main/java/com/ansur/threading/MainThreadImpl.java
+++ app/src/main/java/com/ansur/threading/MainThreadImpl.java
@@ -24,7 +24,7 @@ public class MainThreadImpl implements MainThread {
         if (mainThread == null) {
             mainThread = new MainThreadImpl();
         }
-
+        
         return mainThread;
     }
 }
